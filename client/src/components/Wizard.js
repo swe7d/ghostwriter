b8f85bc85efc1ec10af47aa4057121eb3c390b91
Kaden King <kingk45@ufl.edu>
2019-10-28 17:27:17 -0400

    a state everyone can start working from

--- client/src/components/Wizard.js
+++ client/src/components/Wizard.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react'
 import WizardStepper from './WizardStepper'
 
-export default class Wizard extends Component {
+/**
+ * REFEREENCE STATE:
+ * {
+    basic: {
+        firstname: string,
+        lastname: string,
+        dob: date,
 
+    },
+    milestones: [
+        {
+            id: int,
+            type: string,
+            name: string,
+            start: date,
+            end: date,
+
+
+            // will depend on type of milestone
+            content: {
+                //if milliary:
+                branch: string,
+                rank: string,
+            }
+        },
+    ],
+    design: {
+        order: [
+            //list of ids 
+        ]
+    }
+}
+ * 
+ */
+
+const initialState = {
+    basic: {
+        firstname: "",
+        lastname: "",
+        dob: new Date(),
+
+    },
+    milestones: [],
+    design: {
+        order: []
+    }
+}
+
+export default class Wizard extends Component {
+    state = initialState
+    
     render() {
         return (
             <div>
