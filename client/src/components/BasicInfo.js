f4eb93634cc6e39b63addb8785e36251fa6dc1e7
laradisu <32111302+laradisu@users.noreply.github.com>
2019-10-29 14:14:32 -0400

    basic info

--- client/src/components/BasicInfo.js
+++ client/src/components/BasicInfo.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
 export default class BasicInfo extends Component {
     render() {
@@ -6,7 +10,12 @@ export default class BasicInfo extends Component {
             //basic info
             <div>
                 Basic information component - test pr
+                <p> First Name</p>
+                <p> Last Name</p>
+                <p> DOB </p>
+                <p> Hometown </p>
             </div>
+            
         )
     }
 }
