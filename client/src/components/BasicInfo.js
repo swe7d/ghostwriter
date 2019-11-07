3b212bac8a27cb8a87bcba17ec0c699609f62cb6
laradisu <32111302+laradisu@users.noreply.github.com>
2019-11-06 19:27:09 -0500

    date picker packages  installed

--- client/src/components/BasicInfo.js
+++ client/src/components/BasicInfo.js
@@ -4,6 +4,14 @@ import { makeStyles } from '@material-ui/core/styles';
 //import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
 const useStyles = makeStyles(theme => ({
     container: {
       display: 'flex',
@@ -19,6 +27,12 @@ const useStyles = makeStyles(theme => ({
 export default function BasicInfo() {
     const classes = useStyles();  
 
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2019-11-18T00:00:00'));
+
+    const handleDateChange = date => {
+        setSelectedDate(date);
+    };
+
         return (
             //basic info
             <form className={classes.container} noValidate autoComplete="off">
@@ -29,17 +43,27 @@ export default function BasicInfo() {
                     className={classes.textField}
                     label="Name"
                     margin="normal"
-                    variant="filled"
                     />
                 </div>
                 <div>
-                    <TextField
-                    id="DOB"
-                    className={classes.textField}
-                    label="Date of Birth"
-                    margin="normal"
-                    variant="filled"
-                    />
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <Grid container justify="space-around">
+                        <KeyboardDatePicker
+                        disableToolbar
+                        variant="filled"
+                        format="MM/dd/yyyy"
+                        margin="normal"
+                        id="date"
+                        className={classes.textField}
+                        label="Date"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
+                        />
+                    </Grid>
+                </MuiPickersUtilsProvider>
                 </div>
                 <div>
                     <TextField
@@ -47,7 +71,6 @@ export default function BasicInfo() {
                     className={classes.textField}
                     label="Hometown"
                     margin="normal"
-                    variant="filled"
                     />
                 </div>
                 <div>
@@ -56,7 +79,6 @@ export default function BasicInfo() {
                     className={classes.textField}
                     label="Title of Your Story"
                     margin="normal"
-                    variant="filled"
                     />
                 </div>
             </form>           
