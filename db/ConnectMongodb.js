2b8f9f611c228f5676536193e76b355ccb9b5266
Kaden King <kingk45@ufl.edu>
2019-11-14 23:10:00 -0500

    added dev and prod urls

--- db/ConnectMongodb.js
+++ db/ConnectMongodb.js
@@ -6,8 +6,10 @@ const connect = () => {
     const deployment = process.env.deployment ? process.env.deployment : 'LOCAL'
     if (deployment == 'PROD') {
         console.log('connecting to production database...')
+        return mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
     } else if (deployment == 'DEV') {
         console.log('connecting to development database')
+        return mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
     } else {
         console.log('connecting to local database...')
         const localUrl = 'mongodb://localhost:27017/ghostwriter'
