0b225b5dd48e9eb921fe402dd87d4580b3ee6670
Tuan Le <letuanlt09@gmail.com>
2019-11-23 20:27:27 -0500

    addYoutubeVideo

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
-
+// import {Player,ControlBar} from 'video-react'
+import YouTube from 'react-youtube';
+//https://youtu.be/9WyZWvpZiB4
 class Landing extends Component {
-    render() {  
+    videoOnReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+      }
+      render() {
+        const opts = {
+          height: '390',
+          width: '640',
+          playerVars: { // https://developers.google.com/youtube/player_parameters
+            autoplay: 1
+          }
+        };
+     
         return (
-            <div>
-                landing
-            </div>
-        )
+            <dev>
+                <br></br>
+                <br></br>
+            <YouTube
+            videoId="9WyZWvpZiB4"
+            opts={opts}
+            onReady={this._onReady}
+            />
+            </dev>
+            
+          
+        );
+      }
+     
+      
     }
-}
 
 export default Landing
