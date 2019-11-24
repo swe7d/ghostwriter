9f64e51a159db17e6e458b1e72aa0b75ee199429
Kaden King <kingk45@ufl.edu>
2019-11-24 15:01:26 -0500

    maybe fixed

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,6 @@
 import React from 'react'
-import useAuth from '../hooks/useAuth'
-import hooks from '../hooks/useBooks'
-import { useFirebase } from 'react-redux-firebase'
-
 
 const Landing = () => {
-        console.log(process.env.REACT_APP_API)
-
         return (
             <div>   
                 landing
