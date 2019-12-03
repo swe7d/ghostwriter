e074cf1ba74defafd451eb4e836c290e45e25524
BenjaminECohen <cohenb@ufl.edu>
2019-12-02 23:27:58 -0500

    Implemented new pdf document generator

--- client/src/components/PDFGenerator.js
+++ client/src/components/PDFGenerator.js
@@ -16,7 +16,7 @@ class PDFGenerator extends Component {
         var splitText = "";
         var defaultTextColor = "#000000"
         var watermarkTextColor = "#959595"
-        var watermarkText = "WATERMARK"
+        var watermarkText = "PREVIEW"
         
         doc.setFont('courier');
         doc.setFontType('normal');
@@ -25,57 +25,52 @@ class PDFGenerator extends Component {
         //Add a watermark at start of doc
         doc.setTextColor(watermarkTextColor); //#959595
         doc.setFontSize(140);
-        doc.text(watermarkText, doc.internal.pageSize.getWidth() * (7/8), doc.internal.pageSize.getHeight(), "center", 55);
+        doc.text(watermarkText, doc.internal.pageSize.getWidth() * (3/4), doc.internal.pageSize.getHeight() * (3/4), "center", 55);
                         
         //Reset Text Values
         doc.setTextColor(defaultTextColor);
         doc.setFontSize(16);
         
-        
+        console.log("DATA HERE");
+        console.log(data);
         //splitText = doc.splitTextToSize(data[0].type, 180);
         //doc.text(splitText, 10, 20);
         //doc.addPage("a4", "portrait");
-        /*
+        
         //THIS PART NEEDS TO BE CHANGED
         infoText += basicInfo.firstname + " " + basicInfo.lastname + "\n";
-        infoText += JSON.stringify(basicInfo.selectedDate) +"\n";
-        infoText += "Born in " + basicInfo.hometown + "\n";
         infoText += "\n" + basicInfo.title + " \n\n"
             
         for (var i = 0; i < data.length; i++)
             {
-                infoText += data[i].type + "\n";               
+                //infoText += data[i].type + "\n";               
                 var keys = Object.keys(data[i].data);
                 var values = Object.values(data[i].data);
                 
                 //Print the data keys and values
                 for (var j = 0; j < keys.length; j++)
                     {
-                        infoText += keys[j] + ": ";
-                        infoText += values[j] + "\n";
+                        
+                        infoText += values[j] + "\n\n";
+                        
+                        //USED FOR WHEN PARAGRAPHS WERE ONLY MADE BY ENTIRE MILESTONES
+                        /*if (j == 0)
+                            {
+                                infoText += values[j];
+                            }
+                        else
+                        {
+                            infoText += " " + values[j];
+                        }*/
+                        
                     }
                 //infoText += JSON.stringify(infoText.length)
-                infoText += "\n"
                 
             }
-        
-        splitText = doc.splitTextToSize(infoText, 180);
-        doc.text(splitText, 10, 20);
-        doc.addPage("a4", "portrait");    
-        infoText = "";
-        */
         
         //41 lines at current length: 53 letters per line
         //split text array stores the LINES created in indexes. 
         //Make sure new page pops up if split text array is greater than 42
-        for (var i = 0; i < 70; i++)
-            {
-                for (var j = 0; j < i; j++)
-                    {
-                        infoText += "l"
-                    }
-                infoText += i + "\n";
-            }
         splitText = doc.splitTextToSize(infoText, 180);
         
         //Splitting up into pages
@@ -83,13 +78,15 @@ class PDFGenerator extends Component {
         for (var i = 0; i < splitText.length; i++)
             {
                 tempString += splitText[i] + "\n";
-                if (i % 41 == 0 && i != 0)
+                if (i % 40 == 0 && i != 0)
                     {
                         doc.text(tempString, 10, 20);
                         doc.addPage("a4", "portrait");
                         doc.setTextColor(watermarkTextColor); //#959595
                         doc.setFontSize(140);
-                        doc.text(watermarkText, doc.internal.pageSize.getWidth() * (7/8), doc.internal.pageSize.getHeight(), "center", 55);
+                        doc.text(watermarkText, doc.internal.pageSize.getWidth() * (3/4), doc.internal.pageSize.getHeight() * (3/4), "center", 55);
+                        doc.setFontSize(16);
+                        doc.setTextColor(defaultTextColor);
                         tempString = "";
                     }
                 if (i == splitText.length - 1)
